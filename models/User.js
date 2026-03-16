77af3099439fea1e5136c480dc181696625a1d7c
gilangbagas277 <gilangbagas140303@gmail.com>
2026-03-17 00:42:19 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  expiryDate: { type: Date, required: true },
+  isKicked: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
